e00005e9c4d30c405b04871d4390bbc637afa8f1
shafiqshuvo <shafiq.shuvo.nstu.ice@gmail.com>
2019-10-16 09:18:00 +0600

    vehicle

---
+++ VechicleAppPractice2/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+    public class Vehicle
+    {
+        private string name;
+        private string regNo;
+
+        List<double> speeds = new List<double>();
+
+        public string GetName { set; get;   }
+        public string GetRegNo { set; get; }
+        public double GetSpeed { set; get; }
+
+        public void AddSpeedToList()
+        {
+            speeds.Add(GetSpeed);
+        }
+
+        public double MaxSpeed()
+        {
+            return speeds.Max();
+        }
+
+        public double MinSpeed()
+        {
+            
+                return speeds.Min();
+         
+        }
+
+        public double AverageSpeed()
+        {
+            return speeds.Average();
+        }
+
+    }
+}
---
+++ VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        Vehicle vehicle = new Vehicle();
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            if (! String.IsNullOrEmpty(vehicleNameTextBox.Text) && !String.IsNullOrEmpty(regNoTextBox.Text))
+            {
+                vehicle.GetName = vehicleNameTextBox.Text;
+                vehicle.GetRegNo = regNoTextBox.Text;
+
+                vehicleNameTextBox.Clear();
+                regNoTextBox.Clear();
+
+            }
+            else
+            {
+                MessageBox.Show("Feild Can't be Empty  ");
+            }
+
+            
+
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(speedTextBox.Text))
+            {
+                vehicle.GetSpeed = Convert.ToDouble(speedTextBox.Text);
+                vehicle.AddSpeedToList();
+                speedTextBox.Clear();
+
+            }
+
+            else
+            {
+                MessageBox.Show("Speed Cant be empty ");
+            }
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+           
+                minSpeedTextBox.Text = vehicle.MinSpeed().ToString();
+                maxSpeedTextBox.Text = vehicle.MaxSpeed().ToString();
+                averageSpeedTextBox.Text = vehicle.AverageSpeed().ToString();
+           
+        }
+    }
+
+}
